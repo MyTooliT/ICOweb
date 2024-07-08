984f8782afb88ec388930d08a60c6667fe6c3ab1
Moritz Breurather <breurather@ift.at>
2024-07-08 10:53:25 +0200

    Removed @apply usage for buttons
    In favor over using @apply in tailwind.css, I have switched to using data-* syntax for utility classes. This keeps the focus on utility-first css.

--- src/components/elements/buttons/SolidButton.vue
+++ src/components/elements/buttons/SolidButton.vue
@@ -1,15 +1,30 @@
 <script lang="ts" setup>
 import { ButtonProps } from './types';
 
-const props = withDefaults(defineProps<ButtonProps>(), {
+withDefaults(defineProps<ButtonProps>(), {
   color: 'primary',
 });
 </script>
 
 <template>
+  <!-- eslint-disable max-len  -->
   <button
     :data-color="color"
-    class="data-[color=error]:btn-error data-[color=primary]:btn-primary data-[color=secondary]:btn-secondary data-[color=tertiary]:btn-tertiary border-0 px-4 py-2 transition-all hover:border-0 hover:shadow-xl disabled:hover:shadow-none">
+    class="
+      data-primary:bg-primary data-primary:text-on-primary data-primary:shadow-primary
+      data-primary:disabled:bg-primary-fixed-dim data-primary:disabled:hover:bg-primary-fixed-dim
+
+      data-secondary:bg-secondary data-secondary:text-on-secondary data-secondary:shadow-secondary
+      data-secondary:disabled:bg-secondary-fixed-dim data-secondary:disabled:hover:bg-secondary-fixed-dim
+
+      data-tertiary:bg-tertiary data-tertiary:text-on-tertiary data-tertiary:shadow-tertiary
+      data-tertiary:disabled:bg-tertiary-fixed-dim data-tertiary:disabled:hover:bg-tertiary-fixed-dim
+
+      data-error:bg-error data-error:text-on-error data-error:shadow-error
+      data-error:disabled:bg-error-container data-error:disabled:text-secondary data-error:disabled:hover:bg-error-container
+
+      border-0 px-4 py-2 transition-all
+      hover:border-0 hover:shadow-xl disabled:hover:shadow-none">
     <slot name="left" />
 
     <slot>{{ text }}</slot>
