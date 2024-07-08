0a0b059d1bbce4300476caad1f52534fa7e2bb24
Moritz Breurather <breurather@ift.at>
2024-07-08 12:01:57 +0200

    Moved tailwind css for code analysis exclusion

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import './styles/style.css'
-import './styles/tailwind-output.css'
-import './styles/material-theme/light.css'
-import './styles/material-theme/light-mc.css'
-import './styles/material-theme/light-hc.css'
-import './styles/material-theme/dark.css'
-import './styles/material-theme/dark-mc.css'
-import './styles/material-theme/dark-hc.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import './styles/style.css';
+import './styles/tailwind/tailwind-output.css';
+import './styles/material-theme/light.css';
+import './styles/material-theme/light-mc.css';
+import './styles/material-theme/light-hc.css';
+import './styles/material-theme/dark.css';
+import './styles/material-theme/dark-mc.css';
+import './styles/material-theme/dark-hc.css';
+import App from './App.vue';
 
 const pinia = createPinia();
 const app = createApp(App)
