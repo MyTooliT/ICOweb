b1a83673b492ffbe39dbac09b264d9119065d1b5
Moritz Breurather <breurather@ift.at>
2024-07-08 11:56:05 +0200

    Improved vitest usage

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 /// <reference types="vitest" />>
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from "path"
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   test: {
     includeSource: ['src/**/*.{js,ts,vue}'],
-    environment: 'happy-dom'
+    environment: 'happy-dom',
+    watch: false
   },
   resolve: {
     alias: {
