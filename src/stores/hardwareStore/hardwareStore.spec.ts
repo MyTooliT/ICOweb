9d5a4392b3893f0773286f68f80828b09142f4de
Moritz Breurather <breurather@ift.at>
2024-07-10 08:01:55 +0200

    basic hardwareStore.ts

---
+++ src/stores/hardwareStore/hardwareStore.spec.ts
@@ -0,0 +1,51 @@
+import { createPinia, setActivePinia } from 'pinia';
+import { beforeEach, describe, expect, it } from 'vitest';
+import { useHardwareStore } from './hardwareStore';
+import { Sensor } from './Sensor';
+
+describe('hardwareStore', () => {
+  beforeEach(() => {
+    setActivePinia(createPinia());
+  });
+
+  it('initializes with an empty sensor list', () => {
+    const store = useHardwareStore();
+    expect(store.getSensorList).toEqual([]);
+  });
+
+  it('adds a sensor to the list', () => {
+    const store = useHardwareStore();
+    const sensor = new Sensor('Temperature', '°C', -50, 50);
+    store.addSensor(sensor);
+    expect(store.getSensorList.indexOf(sensor)).toBe(0);
+  });
+
+  it('clears the sensor list', () => {
+    const store = useHardwareStore();
+    const sensor = new Sensor('Temperature', '°C', -50, 50);
+    store.addSensor(sensor);
+    store.clearSensorList();
+    expect(store.getSensorList).toEqual([]);
+  });
+
+  it('removes a sensor from the list', () => {
+    const store = useHardwareStore();
+    const sensor1 = new Sensor('Temperature', '°C', -50, 50);
+    const sensor2 = new Sensor('Pressure', 'Pa', 0, 100);
+    store.addSensor(sensor1);
+    store.addSensor(sensor2);
+    store.removeSensor(sensor1);
+    expect(store.getSensorList.indexOf(sensor1)).toBe(-1)
+    expect(store.getSensorList.indexOf(sensor2)).not.toBe(-1)
+  });
+
+  it('does not remove a sensor that is not in the list', () => {
+    const store = useHardwareStore();
+    const sensor1 = new Sensor('Temperature', '°C', -50, 50);
+    const sensor2 = new Sensor('Pressure', 'Pa', 0, 100);
+    store.addSensor(sensor1);
+    store.removeSensor(sensor2);
+    expect(store.getSensorList.indexOf(sensor1)).not.toBe(-1)
+    expect(store.getSensorList.indexOf(sensor2)).toBe(-1)
+  });
+});
---
+++ src/stores/hardwareStore/hardwareStore.ts
@@ -0,0 +1,24 @@
+import { defineStore } from 'pinia';
+import { computed, ComputedRef, Ref, ref } from 'vue';
+import { Sensor } from './Sensor.ts';
+
+export const useHardwareStore = defineStore('hardware', () => {
+  const _sensorList: Ref<Array<Sensor>> = ref([]);
+  const getSensorList: ComputedRef<Array<Sensor>> = computed(() => {
+    return _sensorList.value
+  })
+  function addSensor(newSensor: Sensor) {
+    _sensorList.value.push(newSensor);
+  }
+  function clearSensorList() {
+    _sensorList.value = []
+  }
+  function removeSensor(sensor: Sensor) {
+    const index = _sensorList.value.indexOf(sensor);
+    if (index > -1) {
+      _sensorList.value.splice(index, 1);
+    }
+  }
+
+  return {getSensorList, addSensor, clearSensorList, removeSensor}
+})
