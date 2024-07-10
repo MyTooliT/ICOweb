a5b865e7bc1fe9a21fe7303d61f799402b09c8c4
Moritz Breurather <breurather@ift.at>
2024-07-10 06:51:02 +0200

    lint fix

--- src/stores/ThemeProvider/themeProvider.ts
+++ src/stores/ThemeProvider/themeProvider.ts
@@ -1,17 +1,18 @@
-import { defineStore } from "pinia";
-import { computed, ref, type Ref, type ComputedRef } from "vue";
+import { defineStore } from 'pinia';
+import { computed, type ComputedRef, type Ref, ref } from 'vue';
 
 export enum ThemeStyle {
-    Light = "light",
-    LightMediumContrast = "light-medium-contrast",
-    LightHighContrast = "light-hight-contrast",
-    Dark = "dark",
-    DarkMediumContrast = "dark-medium-contrast",
-    DarkHighContrast = "dark-high-contrast",
+    Light = 'light',
+    LightMediumContrast = 'light-medium-contrast',
+    LightHighContrast = 'light-hight-contrast',
+    Dark = 'dark',
+    DarkMediumContrast = 'dark-medium-contrast',
+    DarkHighContrast = 'dark-high-contrast',
 }
 
 export const useThemeProviderStore = defineStore('theme', () => {
     const _activeTheme: Ref<ThemeStyle> = ref<ThemeStyle>(ThemeStyle.Light);
+    /* eslint-disable-next-line max-len */
     const getActiveTheme: ComputedRef<ThemeStyle> = computed(() => _activeTheme.value)
     function setActiveTheme(newTheme: ThemeStyle): Boolean {
         if(newTheme !== _activeTheme.value) {
