12f8dc5ca91f01242cf772dd7cbacb87e630a38a
Moritz Breurather <breurather@ift.at>
2024-10-24 10:41:44 +0200

    Frontend: Removed unstable auto-router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,72 @@
+// Import top-level views
+import Analyze from '@/pages/analyze.vue';
+import Config from '@/pages/config.vue';
+
+// Import config child views
+import Sensors from '@/pages/config/sensors.vue';
+import Storage from '@/pages/config/storage.vue';
+import Tools from '@/pages/config/tools.vue';
+import Debug from '@/pages/debug.vue';
+import Help from '@/pages/help.vue';
+import Home from '@/pages/index.vue';
+import Measure from '@/pages/measure.vue';
+import { useGeneralStore } from '@/stores/generalStore/generalStore.ts';
 import {
   createRouter,
   createWebHashHistory
 } from 'vue-router';
-import { routes } from 'vue-router/auto-routes';
-import { useGeneralStore } from '@/stores/generalStore/generalStore.ts';
+
+// Define the routes
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+  },
+  {
+    path: '/analyze',
+    name: 'Analyze',
+    component: Analyze,
+  },
+  {
+    path: '/config',
+    name: 'Config',
+    component: Config,
+    children: [
+      {
+        path: 'sensors',
+        name: 'Sensors',
+        component: Sensors,
+      },
+      {
+        path: 'storage',
+        name: 'Storage',
+        component: Storage,
+      },
+      {
+        path: 'tools',
+        name: 'Tools',
+        component: Tools,
+      },
+    ],
+  },
+  {
+    path: '/debug',
+    name: 'Debug',
+    component: Debug,
+  },
+  {
+    path: '/help',
+    name: 'Help',
+    component: Help,
+  },
+  {
+    path: '/measure',
+    name: 'Measure',
+    component: Measure,
+  },
+];
+
 
 const router = createRouter({
   history: createWebHashHistory(),
