f78a334b76b70a8edceef20f3e3376d33f0ae63a
Moritz Breurather <breurather@ift.at>
2025-03-19 09:50:11 +0100

    Added extra logo option in sidenav

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
-import logo from '@/assets/img/ift_logo.jpg';
 // eslint-disable-next-line max-len
-import RouterLinkButton from '@/components/elements/buttons/RouterLinkButton.vue';
 import BottomBar from '@/components/elements/misc/BottomBar.vue';
 import ThemeProvider from '@/components/elements/misc/ThemeProvider.vue';
 import { useGeneralStore } from '@/stores/generalStore/generalStore.ts';
@@ -9,6 +7,7 @@ import ProgressSpinner from 'primevue/progressspinner';
 import Toast from 'primevue/toast';
 // eslint-disable-next-line max-len
 import NavigationButtons from '@/components/elements/controls/NavigationButtons.vue';
+import NavigationExtras from '@/components/elements/controls/NavigationExtras.vue';
 
 const store = useGeneralStore()
 </script>
@@ -18,18 +17,7 @@ const store = useGeneralStore()
     <div class="flex flex-row h-dvh w-dvw bg-surface overflow-hidden">
       <nav class="bg-surface-container flex flex-col z-50">
         <NavigationButtons />
-        <div class="mt-auto">
-          <RouterLinkButton
-            name="Help"
-            to="/help"
-          >
-            <img
-              :src="logo"
-              alt="IFT Logo"
-              class="w-full max-w-16 mx-auto"
-            >
-          </RouterLinkButton>
-        </div>
+        <NavigationExtras />
       </nav>
       <div class="flex flex-col w-full relative">
         <div
