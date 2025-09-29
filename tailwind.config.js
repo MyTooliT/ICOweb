3b1cb47a055f319f84db7fcb5da9bb58e6214b65
Moritz Breurather <>
2025-09-29 13:31:15 +0200

    Chore: Update Tailwind structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-import { textOrientation, widthStretch, firefox } from '@/utils/tailwindPlugins.js';
+import { textOrientation, widthStretch, firefox } from './src/utils/tailwindPlugins.ts';
 
 export default {
   content: ['./src/**/*.{html,js,vue}'],
