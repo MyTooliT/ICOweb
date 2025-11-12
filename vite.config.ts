9f55d858e9c2bee0c9a400ca27ad06c6aa99c2f3
Moritz Breurather <>
2025-11-12 14:10:37 +0100

    Bugfix: properly use VITE_APPLICATION_PORT for serving

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,34 @@
 /// <reference types="vitest" />>
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  test: {
-    includeSource: ['tests/**/*.{js,ts,vue}'],
-    environment: 'happy-dom',
-    watch: false,
-    setupFiles: [path.resolve(__dirname, 'tests/setupTests.ts')],
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '!@': path.resolve(__dirname, './tests')
+export default ({ mode }: { mode: string }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+  const portEnv = process.env.VITE_APPLICATION_PORT;
+  const port = portEnv ? parseInt(portEnv) : 5173
+
+  return defineConfig({
+    plugins: [vue()],
+    test: {
+      includeSource: ['tests/**/*.{js,ts,vue}'],
+      environment: 'happy-dom',
+      watch: false,
+      setupFiles: [path.resolve(__dirname, 'tests/setupTests.ts')],
     },
-  },
-  define: {
-    '__APP_VERSION__': JSON.stringify(process.env.npm_package_version),
-  }
-})
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+        '!@': path.resolve(__dirname, './tests')
+      },
+    },
+    define: {
+      '__APP_VERSION__': JSON.stringify(process.env.npm_package_version),
+    },
+    server: {
+      port: port
+    }
+  })
+}
